f3a00a8c0aca9784101770eb7677ddd09fc6614e
reynucci <118579889+reynucci@users.noreply.github.com>
2023-07-01 03:11:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const navLinks = document.getElementById('navLinks');
+
+const hideMenu = () => {
+    navLinks.style.right = '-200px';
+}
+const showMenu = () => {
+    navLinks.style.right = '0';
+}
